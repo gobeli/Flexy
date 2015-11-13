8608f046c1a895832fb61eab74369b91ed409433
gobeli <etienne.gobeli@outlook.com>
2015-11-13 20:52:20 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+// Gruntfile.js
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          'dist/grid.css': 'src/grid.scss'
+        }
+      }
+    },
+    
+    autoprefixer:{
+      build:{
+        options: { 
+          style: 'expanded'
+        },
+        files:{
+          'dist/grid.css': 'dist/grid.css'
+        }
+      }
+    },
+
+    cssmin: {
+      build: {
+        files: {
+          'dist/grid.min.css': 'dist/grid.css'
+        }
+      }
+    },
+    
+    watch: {
+      css: {
+        files: ['src/**/*.scss'],
+        tasks: ['sass','autoprefixer','cssmin']
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass','autoprefixer','cssmin','watch']);
+
+};
---
+++ README.md
@@ -0,0 +1,21 @@
+#Flexy
+
+##A Flexbox-based 12 column grid-system
+
+###Media queries
+
+| Small (s)        | Medium (m)       | Large (m)       |
+| ---------------- |:----------------:| ---------------:|
+| max-width: 480px | max-width: 768px | max-width: 980px|
+
+###use
+
+```html
+<div class="row">
+	<div class="col-m2"></div>
+	<div class="col-m2"></div>
+</div>
+<div class="row">
+	<div class="col-m2 col-offset-m2"></div>
+</div>
+```
